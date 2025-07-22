ec8d4b0e55d4d7fb3720f0a0e4c1722fdd256e07
Dumskii Artem <boltushko@gmail.com>
2025-07-23 02:55:18 +0300

    review 1-2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -129,8 +129,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10,
+    'DEFAULT_PAGINATION_CLASS': 'food.pagination.CustomPageNumberPagination',
+    'PAGE_SIZE': 6,
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
     ]
