cd84cebd6ae5b477b493c08fe398d678abda1563
Dumskii Artem <boltushko@gmail.com>
2025-07-23 16:55:26 +0300

    1

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,22 +6,21 @@ from food.views import (FavoriteViewSet, IngredientViewSet, RecipeViewSet,
                         ShoppingCartViewSet, TagViewSet)
 from users.views import CustomUserViewSet
 
-api_v1 = DefaultRouter()
+api = DefaultRouter()
 
-api_v1.register(r'ingredients',
+api.register(r'ingredients',
                 IngredientViewSet, basename='ingredients')
-api_v1.register(r'recipes', RecipeViewSet, basename='recipes')
-api_v1.register(r'tags', TagViewSet, basename='tags')
-api_v1.register(r'users', CustomUserViewSet, basename='users')
-api_v1.register('favorites', FavoriteViewSet, basename='favorites')
-api_v1.register('shopping_cart', ShoppingCartViewSet, basename='shopping-cart')
+api.register(r'recipes', RecipeViewSet, basename='recipes')
+api.register(r'tags', TagViewSet, basename='tags')
+api.register(r'users', CustomUserViewSet, basename='users')
+api.register('favorites', FavoriteViewSet, basename='favorites')
+api.register('shopping_cart', ShoppingCartViewSet, basename='shopping-cart')
 
 
 urlpatterns = [
-    path('users/subscriptions/',
-         CustomUserViewSet.as_view({'get': 'subscriptions'}),
-         name='user-subscriptions'),
-    # path('', include('djoser.urls')),
+    # path('users/subscriptions/',
+    #      CustomUserViewSet.as_view({'get': 'subscriptions'}),
+    #      name='user-subscriptions'),
     path('auth/', include('djoser.urls.authtoken')),
-    path('', include(api_v1.urls)),
+    path('', include(api.urls)),
 ]
