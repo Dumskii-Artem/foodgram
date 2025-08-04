fdff2e8e716b9deba4c51771c11a76d803a97d29
Dumskii Artem <boltushko@gmail.com>
2025-08-04 14:14:58 +0300

    9

--- frontend/src/pages/about/index.js
+++ frontend/src/pages/about/index.js
@@ -38,10 +38,10 @@ const About = ({ updateOrders, orders }) => {
           </h2>
           <div className={styles.text}>
             <p className={styles.textItem}>
-              Код проекта находится тут - <a href="#" className={styles.textLink}>Github</a>
+              Код проекта находится тут - <a href="https://github.com/Dumskii-Artem/foodgram.git" className={styles.textLink}>Github</a>
             </p>
             <p className={styles.textItem}>
-              Автор проекта: <a href="#" className={styles.textLink}>Имя Автора</a>
+              Автор проекта: <a href="https://github.com/Dumskii-Artem" className={styles.textLink}>Думский Артём</a>
             </p>
           </div>
         </aside>
